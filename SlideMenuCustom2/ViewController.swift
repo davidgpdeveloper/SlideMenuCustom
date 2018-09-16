4c9b0f48b3b8141418712d5e1a0c50c0309269cd
David Galán <david.galan@studiogenesis.es>
2018-09-16 12:59:32 +0200

    Initial Commit

---
+++ SlideMenuCustom2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuCustom2
+//
+//  Created by Studiogenesis Home on 16/9/18.
+//  Copyright © 2018 Studiogenesis Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
