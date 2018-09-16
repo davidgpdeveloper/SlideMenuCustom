cb20e87d5a99fa6eac8392aa8661068a47061696
Studiogenesis Home <studiogenesishome@Davids-Mac.local>
2018-09-16 18:10:41 +0200

    first commit

---
+++ SlideMenuCustom2/MenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.swift
+//  SlideMenuCustom2
+//
+//  Created by Studiogenesis Home on 16/9/18.
+//  Copyright © 2018 Studiogenesis Home. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
